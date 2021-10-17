ae56a917fa1c705039bdefcb68e48a7e529f9954
eva <83175995+evapopp@users.noreply.github.com>
2021-10-16 17:26:31 -0700

    figuring out which css tools to use

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Hero from './Hero';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Hero/>
       test
       </header>
     </div>
